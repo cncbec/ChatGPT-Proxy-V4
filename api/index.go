795764c7781cdc14b6d70dbb5ab0f83a335f30af
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-24 18:18:15 +0800

    Update index.go proxy使用匿名函数代替，解决Handler两个函数问题

--- api/index.go
+++ api/index.go
@@ -26,6 +26,8 @@ var (
 	http_proxy = os.Getenv("http_proxy")
 )
 
+
+
 func Handler(w http.ResponseWriter, r *http.Request) {
 
 	if http_proxy != "" {
@@ -42,7 +44,76 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		c.JSON(200, gin.H{"message": "pong"})
 	})
 
-	handlerss.Any("/api/*path", proxy)
+	handlerss.Any("/api/*path", func (c *gin.Context) {
+		// Remove _cfuvid cookie from session
+		jar.SetCookies(c.Request.URL, []*http.Cookie{})
+
+		var url string
+		var err error
+		var request_method string
+		var request *http.Request
+		var response *http.Response
+
+		if c.Request.URL.RawQuery != "" {
+			url = "https://chat.openai.com/backend-api" + c.Param("path") + "?" + c.Request.URL.RawQuery
+		} else {
+			url = "https://chat.openai.com/backend-api" + c.Param("path")
+		}
+		request_method = c.Request.Method
+
+		request, err = http.NewRequest(request_method, url, c.Request.Body)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		request.Header.Set("Host", "chat.openai.com")
+		request.Header.Set("Origin", "https://chat.openai.com/chat")
+		request.Header.Set("Connection", "keep-alive")
+		request.Header.Set("Content-Type", "application/json")
+		request.Header.Set("Keep-Alive", "timeout=360")
+		request.Header.Set("Authorization", c.Request.Header.Get("Authorization"))
+		request.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
+		request.Header.Set("sec-ch-ua-mobile", "?0")
+		request.Header.Set("sec-ch-ua-platform", "\"Linux\"")
+		request.Header.Set("sec-fetch-dest", "empty")
+		request.Header.Set("sec-fetch-mode", "cors")
+		request.Header.Set("sec-fetch-site", "same-origin")
+		request.Header.Set("sec-gpc", "1")
+		request.Header.Set("user-agent", user_agent)
+		if os.Getenv("PUID") != "" {
+			request.AddCookie(&http.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
+		}
+
+		response, err = client.Do(request)
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		defer response.Body.Close()
+		c.Header("Content-Type", response.Header.Get("Content-Type"))
+		// Get status code
+		c.Status(response.StatusCode)
+
+		buf := make([]byte, 4096)
+		for {
+			n, err := response.Body.Read(buf)
+			if n > 0 {
+				_, writeErr := c.Writer.Write(buf[:n])
+				if writeErr != nil {
+					log.Printf("Error writing to client: %v", writeErr)
+					break
+				}
+				c.Writer.Flush() // flush buffer to make sure the data is sent to client in time.
+			}
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Printf("Error reading from response body: %v", err)
+				break
+			}
+		}
+	})
 
 	gin.SetMode(gin.ReleaseMode)
 	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handlerss)

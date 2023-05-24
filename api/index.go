74d30d029ad0135235e3ee336b4a6abdb0f1d958
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-24 17:44:19 +0800

    Update index.go one func

--- api/index.go
+++ api/index.go
@@ -1,10 +1,51 @@
 package api
 
 import (
-  "fmt"
-  "net/http"
+	"io"
+	"log"
+	"os"
+
+	http "github.com/bogdanfinn/fhttp"
+	tls_client "github.com/bogdanfinn/tls-client"
+
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
- 
+
+var (
+	jar     = tls_client.NewCookieJar()
+	options = []tls_client.HttpClientOption{
+		tls_client.WithTimeoutSeconds(360),
+		tls_client.WithClientProfile(tls_client.Chrome_112),
+		tls_client.WithNotFollowRedirects(),
+		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
+	}
+	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+	http_proxy = os.Getenv("http_proxy")
+)
+
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+
+	if http_proxy != "" {
+		client.SetProxy(http_proxy)
+		println("Proxy set:" + http_proxy)
+	}
+
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		PORT = "9090"
+	}
+	handlerss := gin.Default()
+	handlerss.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "pong"})
+	})
+
+	handlerss.Any("/api/*path", proxy)
+
+	gin.SetMode(gin.ReleaseMode)
+	endless.ListenAndServe(os.Getenv("HOST")+":"+PORT, handlerss)
 }
+

5d40afa6ef20346165a76d3aa2d650e71e0fe6c1
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-24 18:06:39 +0800

    Update common.go

--- api/common.go
+++ api/common.go
@@ -1,30 +1,6 @@
 package api
 
-import (
-	"io"
-	"log"
-	"os"
 
-	http "github.com/bogdanfinn/fhttp"
-	tls_client "github.com/bogdanfinn/tls-client"
-
-	"github.com/fvbock/endless"
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
-
-var (
-	jar     = tls_client.NewCookieJar()
-	options = []tls_client.HttpClientOption{
-		tls_client.WithTimeoutSeconds(360),
-		tls_client.WithClientProfile(tls_client.Chrome_112),
-		tls_client.WithNotFollowRedirects(),
-		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
-	}
-	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
-	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-	http_proxy = os.Getenv("http_proxy")
-)
 
 func proxy(c *gin.Context) {
 	// Remove _cfuvid cookie from session

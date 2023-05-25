956f749ec25068c71a694c51d6874e73d3c3d27d
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-25 17:19:50 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -5,30 +5,32 @@ import (
 	"log"
 	"net/http"
 	"os"
-	tls_client "github.com/bogdanfinn/tls-client"
+
+	"github.com/bogdanfinn/fhttp"
+	"github.com/bogdanfinn/tls-client"
 )
 
 var (
-	jar     = tls_client.NewCookieJar()
-	options = []tls_client.HttpClientOption{
+	UserAgent  = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+	HTTPProxy  = os.Getenv("http_proxy")
+	CookieJar  = tls_client.NewCookieJar()
+	ClientOpts = []tls_client.HttpClientOption{
 		tls_client.WithTimeoutSeconds(360),
 		tls_client.WithClientProfile(tls_client.Chrome_112),
 		tls_client.WithNotFollowRedirects(),
-		tls_client.WithCookieJar(jar),
+		tls_client.WithCookieJar(CookieJar),
 	}
-	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
-	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 )
 
-func proxyHandler(w http.ResponseWriter, r *http.Request) {
+type ProxyHandler struct{}
+
+func (h *ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Remove _cfuvid cookie from session
-	jar.SetCookies(r.URL, []*http.Cookie{})
+	CookieJar.SetCookies(r.URL, []*fhttp.Cookie{})
 
 	var url string
 	var err error
 	var requestMethod string
-	var request *http.Request
-	var response *http.Response
 
 	if r.URL.RawQuery != "" {
 		url = "https://chat.openai.com/backend-api" + r.URL.Path + "?" + r.URL.RawQuery
@@ -37,7 +39,7 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	requestMethod = r.Method
 
-	request, err = http.NewRequest(requestMethod, url, r.Body)
+	request, err := fhttp.NewRequest(requestMethod, url, r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -55,12 +57,18 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	request.Header.Set("sec-fetch-mode", "cors")
 	request.Header.Set("sec-fetch-site", "same-origin")
 	request.Header.Set("sec-gpc", "1")
-	request.Header.Set("user-agent", user_agent)
+	request.Header.Set("User-Agent", UserAgent)
 	if os.Getenv("PUID") != "" {
-		request.AddCookie(&http.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
+		request.AddCookie(&fhttp.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
 	}
 
-	response, err = client.Do(request)
+	client, err := fhttp.NewHttpClient(fhttp.NewNoopLogger(), ClientOpts...)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response, err := client.Do(request)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
@@ -78,7 +86,7 @@ func proxyHandler(w http.ResponseWriter, r *http.Request) {
 				log.Printf("Error writing to client: %v", writeErr)
 				break
 			}
-			w.(http.Flusher).Flush() // flush buffer to make sure the data is sent to client in time.
+			w.(http.Flusher).Flush()
 		}
 		if err == io.EOF {
 			break
@@ -96,13 +104,15 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(`{"message": "pong"}`))
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/api/":
-		proxyHandler(w, r)
-	case "/ping":
-		pingHandler(w, r)
-	default:
-		http.NotFound(w, r)
+func main() {
+	if HTTPProxy != "" {
+		tls_client.DefaultTransport.SetProxy(HTTPProxy)
+		log.Println("Proxy set:", HTTPProxy)
 	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/api/", &ProxyHandler{})
+	mux.HandleFunc("/ping", pingHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }

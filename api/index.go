b4ea9dd7baca87df3d0a20d96a082a954cfa7302
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-25 16:46:40 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -3,9 +3,8 @@ package api
 import (
 	"io"
 	"log"
+	"net/http"
 	"os"
-
-	"github.com/bogdanfinn/fhttp"
 	tls_client "github.com/bogdanfinn/tls-client"
 )
 
@@ -21,65 +20,65 @@ var (
 	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 )
 
-func proxy(c fhttp.Context) {
+func proxyHandler(w http.ResponseWriter, r *http.Request) {
 	// Remove _cfuvid cookie from session
-	jar.SetCookies(c.Request().URL(), []*fhttp.Cookie{})
+	jar.SetCookies(r.URL, []*http.Cookie{})
 
 	var url string
 	var err error
-	var request_method string
-	var request *fhttp.Request
-	var response *fhttp.Response
+	var requestMethod string
+	var request *http.Request
+	var response *http.Response
 
-	if c.Request().URL().RawQuery != "" {
-		url = "https://chat.openai.com/backend-api" + c.Param("path") + "?" + c.Request().URL().RawQuery
+	if r.URL.RawQuery != "" {
+		url = "https://chat.openai.com/backend-api" + r.URL.Path + "?" + r.URL.RawQuery
 	} else {
-		url = "https://chat.openai.com/backend-api" + c.Param("path")
+		url = "https://chat.openai.com/backend-api" + r.URL.Path
 	}
-	request_method = c.Request().Method()
+	requestMethod = r.Method
 
-	request, err = fhttp.NewRequest(request_method, url, c.Request().Body())
+	request, err = http.NewRequest(requestMethod, url, r.Body)
 	if err != nil {
-		c.JSON(500, fhttp.Map{"error": err.Error()})
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	request.Header().Set("Host", "chat.openai.com")
-	request.Header().Set("Origin", "https://chat.openai.com/chat")
-	request.Header().Set("Connection", "keep-alive")
-	request.Header().Set("Content-Type", "application/json")
-	request.Header().Set("Keep-Alive", "timeout=360")
-	request.Header().Set("Authorization", c.Request().Header().Get("Authorization"))
-	request.Header().Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
-	request.Header().Set("sec-ch-ua-mobile", "?0")
-	request.Header().Set("sec-ch-ua-platform", "\"Linux\"")
-	request.Header().Set("sec-fetch-dest", "empty")
-	request.Header().Set("sec-fetch-mode", "cors")
-	request.Header().Set("sec-fetch-site", "same-origin")
-	request.Header().Set("sec-gpc", "1")
-	request.Header().Set("user-agent", user_agent)
+	request.Header.Set("Host", "chat.openai.com")
+	request.Header.Set("Origin", "https://chat.openai.com/chat")
+	request.Header.Set("Connection", "keep-alive")
+	request.Header.Set("Content-Type", "application/json")
+	request.Header.Set("Keep-Alive", "timeout=360")
+	request.Header.Set("Authorization", r.Header.Get("Authorization"))
+	request.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
+	request.Header.Set("sec-ch-ua-mobile", "?0")
+	request.Header.Set("sec-ch-ua-platform", "\"Linux\"")
+	request.Header.Set("sec-fetch-dest", "empty")
+	request.Header.Set("sec-fetch-mode", "cors")
+	request.Header.Set("sec-fetch-site", "same-origin")
+	request.Header.Set("sec-gpc", "1")
+	request.Header.Set("user-agent", user_agent)
 	if os.Getenv("PUID") != "" {
-		request.AddCookie(&fhttp.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
+		request.AddCookie(&http.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
 	}
 
 	response, err = client.Do(request)
 	if err != nil {
-		c.JSON(500, fhttp.Map{"error": err.Error()})
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	defer response.Body().Close()
-	c.SetHeader("Content-Type", response.Header().Get("Content-Type"))
-	c.SetStatus(response.StatusCode())
+	defer response.Body.Close()
+	w.Header().Set("Content-Type", response.Header.Get("Content-Type"))
+	w.WriteHeader(response.StatusCode)
 
 	buf := make([]byte, 4096)
 	for {
-		n, err := response.Body().Read(buf)
+		n, err := response.Body.Read(buf)
 		if n > 0 {
-			_, writeErr := c.Write(buf[:n])
+			_, writeErr := w.Write(buf[:n])
 			if writeErr != nil {
 				log.Printf("Error writing to client: %v", writeErr)
 				break
 			}
-			c.Flush() // flush buffer to make sure the data is sent to client in time.
+			w.(http.Flusher).Flush() // flush buffer to make sure the data is sent to client in time.
 		}
 		if err == io.EOF {
 			break
@@ -91,13 +90,19 @@ func proxy(c fhttp.Context) {
 	}
 }
 
-func Handler(c fhttp.Context) error {
-	path := c.Param("path")
-	if path == "ping" {
-		c.JSON(200, fhttp.Map{"message": "pong"})
-		return nil
-	}
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message": "pong"}`))
+}
 
-	proxy(c)
-	return nil
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/api/":
+		proxyHandler(w, r)
+	case "/ping":
+		pingHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
 }

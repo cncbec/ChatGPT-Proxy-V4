ae7b7d4967fea23d882e9b2fa617de039343aa51
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-25 18:39:55 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -3,116 +3,92 @@ package api
 import (
 	"io"
 	"log"
-	"net/http"
 	"os"
 
-	"github.com/bogdanfinn/fhttp"
-	"github.com/bogdanfinn/tls-client"
+	"net/http"
+	tls_client "github.com/bogdanfinn/tls-client"
+
+
 )
 
 var (
-	UserAgent  = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-	HTTPProxy  = os.Getenv("http_proxy")
-	CookieJar  = tls_client.NewCookieJar()
-	ClientOpts = []tls_client.HttpClientOption{
+	jar     = tls_client.NewCookieJar()
+	options = []tls_client.HttpClientOption{
 		tls_client.WithTimeoutSeconds(360),
 		tls_client.WithClientProfile(tls_client.Chrome_112),
 		tls_client.WithNotFollowRedirects(),
-		tls_client.WithCookieJar(CookieJar),
+		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
 	}
+	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+	http_proxy = os.Getenv("http_proxy")
 )
 
+func Proxy(w http.ResponseWriter, r *http.Request) {
+	client := http.DefaultClient
 
-
-func ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Remove _cfuvid cookie from session
-	CookieJar.SetCookies(r.URL, []*fhttp.Cookie{})
+	jar, _ := r.Cookie("_puid")
+	jar.Value = "" // 清空 cookie 的值
+	r.AddCookie(jar)
 
 	var url string
-	var err error
-	var requestMethod string
-
 	if r.URL.RawQuery != "" {
 		url = "https://chat.openai.com/backend-api" + r.URL.Path + "?" + r.URL.RawQuery
 	} else {
 		url = "https://chat.openai.com/backend-api" + r.URL.Path
 	}
-	requestMethod = r.Method
 
-	request, err := fhttp.NewRequest(requestMethod, url, r.Body)
+	reqURL, _ := url.Parse(url)
+	jar.SetCookies(reqURL, []*http.Cookie{})
+
+	req, err := http.NewRequest(r.Method, url, r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	request.Header.Set("Host", "chat.openai.com")
-	request.Header.Set("Origin", "https://chat.openai.com/chat")
-	request.Header.Set("Connection", "keep-alive")
-	request.Header.Set("Content-Type", "application/json")
-	request.Header.Set("Keep-Alive", "timeout=360")
-	request.Header.Set("Authorization", r.Header.Get("Authorization"))
-	request.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
-	request.Header.Set("sec-ch-ua-mobile", "?0")
-	request.Header.Set("sec-ch-ua-platform", "\"Linux\"")
-	request.Header.Set("sec-fetch-dest", "empty")
-	request.Header.Set("sec-fetch-mode", "cors")
-	request.Header.Set("sec-fetch-site", "same-origin")
-	request.Header.Set("sec-gpc", "1")
-	request.Header.Set("User-Agent", UserAgent)
+	req.Host = "chat.openai.com"
+	req.Header.Set("User-Agent", http_proxy)
+	req.Header.Set("Host", "chat.openai.com")
+	req.Header.Set("Origin", "https://chat.openai.com/chat")
+	req.Header.Set("Connection", "keep-alive")
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Keep-Alive", "timeout=360")
+	req.Header.Set("Authorization", r.Request.Header.Get("Authorization"))
+	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
+	req.Header.Set("sec-ch-ua-mobile", "?0")
+	req.Header.Set("sec-ch-ua-platform", "\"Linux\"")
+	req.Header.Set("sec-fetch-dest", "empty")
+	req.Header.Set("sec-fetch-mode", "cors")
+	req.Header.Set("sec-fetch-site", "same-origin")
+	req.Header.Set("sec-gpc", "1")
+	req.Header.Set("user-agent", user_agent)
+
+	// 将环境变量 "PUID" 的值作为 Cookie 添加到请求中
 	if os.Getenv("PUID") != "" {
-		request.AddCookie(&fhttp.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
+		req.AddCookie(&http.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
 	}
 
-	client, err := fhttp.NewHttpClient(fhttp.NewNoopLogger(), ClientOpts...)
+	resp, err := client.Do(req)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+	defer resp.Body.Close()
 
-	response, err := client.Do(request)
+	// 设置响应头部
+	for key, values := range resp.Header {
+		for _, value := range values {
+			w.Header().Add(key, value)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+
+	// 将响应内容拷贝到客户端的响应中
+	_, err = io.Copy(w, resp.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer response.Body.Close()
-	w.Header().Set("Content-Type", response.Header.Get("Content-Type"))
-	w.WriteHeader(response.StatusCode)
-
-	buf := make([]byte, 4096)
-	for {
-		n, err := response.Body.Read(buf)
-		if n > 0 {
-			_, writeErr := w.Write(buf[:n])
-			if writeErr != nil {
-				log.Printf("Error writing to client: %v", writeErr)
-				break
-			}
-			w.(http.Flusher).Flush()
-		}
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Printf("Error reading from response body: %v", err)
-			break
-		}
+		log.Printf("Error copying response body to client: %v", err)
 	}
 }
 
-func pingHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "pong"}`))
-}
 
-func main() {
-	if HTTPProxy != "" {
-		tls_client.DefaultTransport.SetProxy(HTTPProxy)
-		log.Println("Proxy set:", HTTPProxy)
-	}
 
-	mux := http.NewServeMux()
-	mux.Handle("/api/", &ProxyHandler{})
-	mux.HandleFunc("/ping", pingHandler)
-
-	log.Fatal(http.ListenAndServe(":8080", mux))
-}

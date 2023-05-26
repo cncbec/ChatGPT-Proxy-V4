6f4b9f4e69a4032cbec66cf6f8d2a1667f295493
cncbec <54312958+cncbec@users.noreply.github.com>
2023-05-26 10:15:53 +0800

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,59 +1,49 @@
 package api
 
 import (
-	"io"
-	"log"
-	"os"
+    "fmt"
+    "io"
+    "net/http"
 
-	"net/http"
-	tls_client "github.com/bogdanfinn/tls-client"
-
-
+    "github.com/bogdanfinn/tls_client"
 )
 
-var (
-	jar     = tls_client.NewCookieJar()
-	options = []tls_client.HttpClientOption{
-		tls_client.WithTimeoutSeconds(360),
-		tls_client.WithClientProfile(tls_client.Chrome_112),
-		tls_client.WithNotFollowRedirects(),
-		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
-	}
-	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
-	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-	http_proxy = os.Getenv("http_proxy")
-)
+func ForwardRequest(w http.ResponseWriter, r *http.Request) {
+    // 建立安全链接的 transport 对象。
+    transport, err := tls_client.NewTransport(tls_client.TLSConfig{})
+    if err != nil {
+        fmt.Printf("Failed to create HTTPS transport due to %v\n", err)
+        return
+    }
 
-func Proxy(w http.ResponseWriter, r *http.Request) {
-	client := http.DefaultClient
-
-	jar, _ := r.Cookie("_puid")
-	jar.Value = "" // 清空 cookie 的值
-	r.AddCookie(jar)
-
-	var url string
+    // 使用 tls_client 提供的 NewCookieJar 函数创建 cookie jar。
+    cookieJar, err := tls_client.NewCookieJar()
+    if err != nil {
+        fmt.Printf("Failed to create cookie jar due to %v\n", err)
+        return
+    }
+    client := &http.Client{
+        Transport: transport,
+        Jar:       cookieJar,
+    }
+	
 	if r.URL.RawQuery != "" {
 		url = "https://chat.openai.com/backend-api" + r.URL.Path + "?" + r.URL.RawQuery
 	} else {
 		url = "https://chat.openai.com/backend-api" + r.URL.Path
 	}
 
-	reqURL, _ := url.Parse(url)
-	jar.SetCookies(reqURL, []*http.Cookie{})
-
-	req, err := http.NewRequest(r.Method, url, r.Body)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	req.Host = "chat.openai.com"
-	req.Header.Set("User-Agent", http_proxy)
+    // 建立请求对象并设置 headers，包括 Origin、Connection、User-Agent 和 Authorization 等。
+    req, err := http.NewRequest(http.MethodGet, url, nil)
+    if err != nil {
+        fmt.Printf("Failed to create the request object due to %v\n", err)
+        return
+    }
 	req.Header.Set("Host", "chat.openai.com")
 	req.Header.Set("Origin", "https://chat.openai.com/chat")
 	req.Header.Set("Connection", "keep-alive")
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Keep-Alive", "timeout=360")
-	req.Header.Set("Authorization", r.Request.Header.Get("Authorization"))
 	req.Header.Set("sec-ch-ua", "\"Chromium\";v=\"112\", \"Brave\";v=\"112\", \"Not:A-Brand\";v=\"99\"")
 	req.Header.Set("sec-ch-ua-mobile", "?0")
 	req.Header.Set("sec-ch-ua-platform", "\"Linux\"")
@@ -61,34 +51,42 @@ func Proxy(w http.ResponseWriter, r *http.Request) {
 	req.Header.Set("sec-fetch-mode", "cors")
 	req.Header.Set("sec-fetch-site", "same-origin")
 	req.Header.Set("sec-gpc", "1")
-	req.Header.Set("user-agent", user_agent)
+    req.Header.Set("Connection", "keep-alive")
+    req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36")
 
-	// 将环境变量 "PUID" 的值作为 Cookie 添加到请求中
-	if os.Getenv("PUID") != "" {
-		req.AddCookie(&http.Cookie{Name: "_puid", Value: os.Getenv("PUID")})
-	}
+    // 这里假设您已经将源站点的 Authorization 放入自定义头中（Authorization），然后将该头信息转发给目标站点。
+    req.Header.Set("Authorization", r.Header.Get("Authorization"))
 
-	resp, err := client.Do(req)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	defer resp.Body.Close()
+    // 使用刚才创建的 HTTP client 执行请求。
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Printf("Failed to send the request to the target site due to %v\n", err)
+        return
+    }
 
-	// 设置响应头部
-	for key, values := range resp.Header {
-		for _, value := range values {
-			w.Header().Add(key, value)
-		}
-	}
-	w.WriteHeader(resp.StatusCode)
-
-	// 将响应内容拷贝到客户端的响应中
-	_, err = io.Copy(w, resp.Body)
-	if err != nil {
-		log.Printf("Error copying response body to client: %v", err)
-	}
+    // 传递响应 headers 和 body 至调用方，并使用 HTTP 状态码处理响应结果。
+    for key, value := range resp.Header {
+        for _, hValue := range value {
+            w.Header().Add(key, hValue)
+        }
+    }
+    defer resp.Body.Close()
+    w.WriteHeader(resp.StatusCode)
+    buf := make([]byte, 1024)
+    // 将响应体写入流式响应中
+    for {
+        n, err := resp.Body.Read(buf)
+        if err != nil && err != io.EOF {
+            fmt.Printf("Failed to read response Body due to %v\n", err)
+            break
+        }
+        if n == 0 {
+            break
+        }
+        _, err = w.Write(buf[:n])
+        if err != nil {
+            fmt.Printf("Failed to write response Body due to %v\n", err)
+            break
+        }
+    }
 }
-
-
-
